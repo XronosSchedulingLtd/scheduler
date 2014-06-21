3b6612378fe72dc0cc2b4da7586b55aa3904b153
John Winters <john@sinodun.org.uk>
2014-06-21 08:43:01 +0100

    Added licence text to main files.

--- app/models/concerns/grouping.rb
+++ app/models/concerns/grouping.rb
@@ -1,3 +1,9 @@
+# Xronos Scheduler - structured scheduling program.
+# Copyright (C) 2009-2014 John Winters
+# Portions Copyright (C) 2014 Abindon School
+# See COPYING and LICENCE in the root directory of the application
+# for more information.
+
 module Grouping
   extend ActiveSupport::Concern
 
