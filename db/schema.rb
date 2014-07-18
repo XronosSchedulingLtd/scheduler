e64e5b44a7f3b89eb64c270df31624ee9881151e
John Winters <john@sinodun.org.uk>
2014-07-18 13:41:19 +0100

    Suppres menus for non admin users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140716112630) do
+ActiveRecord::Schema.define(version: 20140718123610) do
 
   create_table "commitments", force: true do |t|
     t.integer "event_id"
@@ -193,6 +193,7 @@ ActiveRecord::Schema.define(version: 20140716112630) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "admin",      default: false
   end
 
 end
