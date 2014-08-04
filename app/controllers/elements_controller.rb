ac4d19dd847a1d7827517dd6bdb9170f79b4c1dd
John Winters <john@sinodun.org.uk>
2014-08-04 17:38:58 +0100

    Search on any part of element name.

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,5 +1,5 @@
 class ElementsController < ApplicationController
-  autocomplete :element, :name, :scopes => [:current]
+  autocomplete :element, :name, :scopes => [:current], :full => true
 
   def authorized?(action = action_name, resource = nil)
     logged_in? && current_user.known?
