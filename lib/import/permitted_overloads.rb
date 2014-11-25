868017da6cdd9e4ea844118eb57d4ef524d8a0a8
John Winters <john@sinodun.org.uk>
2014-11-25 08:47:36 +0000

    Added 4th year PSHCE to permitted overloads.

--- lib/import/permitted_overloads.rb
+++ lib/import/permitted_overloads.rb
@@ -8,6 +8,7 @@ PermittedOverload = Struct.new(:cover_event_body, :clash_event_body)
 
 PERMITTED_OVERLOADS = [
   PermittedOverload.new(/^S3 PSHCE/, /^S3 PSHCE/),
+  PermittedOverload.new(/^S4 PSHCE/, /^S4 PSHCE/),
   PermittedOverload.new(/^S6 GSCS/,  /^S6 GSCS/),
   PermittedOverload.new(/1.*BtB/,    /S1 H BtB/),
   PermittedOverload.new(/2.*BtB/,    /S2 H BtB/)
