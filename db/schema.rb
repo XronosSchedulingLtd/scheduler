418e380825457519e2341641ce64cd132dde1bf5
John Winters <john@sinodun.org.uk>
2015-03-31 15:16:58 +0100

    And the resultant schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150330054317) do
+ActiveRecord::Schema.define(version: 20150331175206) do
 
   create_table "commitments", force: true do |t|
     t.integer "event_id"
