286d4a83f8ad3b0c294db786752353363097cfe2
John Winters <john@sinodun.org.uk>
2016-07-23 14:36:15 +0100

    Delay identifying tutor id for tutor group.
    Hit a problem with when the loader was trying to find the
    database record for the tutor to attach to a tutor group.
    It was doing it at initialisation time, and if the tutor
    was a brand new member of staff then it failed because he
    hadn't been loaded yet.
    
    Now delay that step until we're actually loading the tutor
    groups, by which time staff records have been created.

--- lib/import/misimport/mistutorgroup.rb
+++ lib/import/misimport/mistutorgroup.rb
@@ -5,7 +5,7 @@ class MIS_Tutorgroup < MIS_Group
   FIELDS_TO_CREATE = [:name, :era_id, :staff_id, :house, :start_year, :current]
   FIELDS_TO_UPDATE = [:name, :era_id, :staff_id, :house, :start_year, :current]
 
-  attr_reader :staff_id, :staff, :pupils
+  attr_reader :staff, :pupils
 
   def add_pupil(pupil)
     @pupils << pupil
@@ -17,7 +17,14 @@ class MIS_Tutorgroup < MIS_Group
 
   def note_staff(staff)
     @staff = staff
-    @staff_id = staff.dbrecord.id
+  end
+
+  def staff_id
+    if @staff.dbrecord
+      @staff.dbrecord.id
+    else
+      nil
+    end
   end
 
   def size
