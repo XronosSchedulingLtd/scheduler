63f56f7a303db3d22f9242257b72c07af27b987b
John Winters <john@sinodun.org.uk>
2016-08-27 16:29:28 +0100

    Deal with invalid ICS requests for "/"
    It seems that someone has configured their calendar program to
    try to fetch a calendar from just scheduler.abingdon.org.uk
    without adding the rest of the URL.  This leads to a messy
    crash message.  Added code to deal with it more cleanly.

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -216,6 +216,9 @@ class ScheduleController < ApplicationController
       start_at = session[:last_start_date] || Time.zone.now
       @default_date = start_at.strftime("%Y-%m-%d")
       @show_jump = true
+      respond_to do |format|
+        format.html
+      end
     end
   end
 
