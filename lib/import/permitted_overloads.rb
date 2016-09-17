6d3a664ed8ce11c29f91f8c7a99ba264cfef0db0
John Winters <john@sinodun.org.uk>
2016-09-17 10:00:53 +0100

    Add more permitted overloads for cover.

--- lib/import/permitted_overloads.rb
+++ lib/import/permitted_overloads.rb
@@ -15,7 +15,12 @@ PERMITTED_OVERLOADS = [
   PermittedOverload.new(/^S3 PE /,   /^S3 PE /),
   PermittedOverload.new(/^S4 PE /,   /^S4 PE /),
   PermittedOverload.new(/^S5 PE /,   /^S5 PE /),
+  PermittedOverload.new(/^S5 Study Skills/,   /^S5 Study Skills/),
   PermittedOverload.new(/^S6 GSCS/,  /^S6 GSCS/),
+  PermittedOverload.new(/^S6 Spt/,  /^S7 Spt/),
+  PermittedOverload.new(/^S7 Spt/,  /^S6 Spt/),
+  PermittedOverload.new(/^S1 Spt/,  /^S2 Spt/),
+  PermittedOverload.new(/^S2 Spt/,  /^S1 Spt/),
   PermittedOverload.new(/1.*BtB/,    /S1 H BtB/),
   PermittedOverload.new(/2.*BtB/,    /S2 H BtB/)
 ]
