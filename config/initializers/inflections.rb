151f336b02061a8210f03a359e19e81c54cd16ea
John Winters <john@sinodun.org.uk>
2017-12-10 09:22:10 +0000

    After running rails:update
    Ran rake rails:update, and let all files be over-written.  This
    is the state of things before I started re-instating my changes
    to suit the application.

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -14,8 +14,3 @@
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.acronym 'RESTful'
 # end
-
-ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.plural /(persona)$/i, '\1e'
-  inflect.singular /(persona)e$/i, '\1'
-end
