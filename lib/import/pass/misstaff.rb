f14cf20bcf2107f4f0d621828791878ca705403b
John Winters <john@xronos.uk>
2018-06-12 11:47:23 +0100

    Add means to load extra staff.

--- lib/import/pass/misstaff.rb
+++ lib/import/pass/misstaff.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 class MIS_Staff
 
   attr_reader :source_id,
@@ -10,8 +12,7 @@ class MIS_Staff
               :email,
               :formal_name
 
-  def initialize(record)
-    super
+  def from_pass(record)
     #
     #  Don't have a proper source of staff names yet so we'll kind
     #  of busk it for now.
@@ -26,6 +27,20 @@ class MIS_Staff
     @title              = record.label_salutation.partition(" ").first
     @initials           = "#{@forename[0].upcase}#{@surname[0].upcase}"
     @email              = construct_email(forename, surname)
+    self
+  end
+
+  def from_yaml(record, datasource_id)
+    @datasource_id = datasource_id
+    @source_id     = record["source_id"]
+    @formal_name   = record["formal_name"]
+    @forename      = record["forename"]     || "Unknown"
+    @surname       = record["surname"]      || "Unknown"
+    @title         = record["title"]        || ""
+    @initials      = record["initials"]     || ""
+    @email         = record["email"]        || ""
+    @name          = "#{@forename} #{@surname}"
+    self
   end
 
   def split_name(tutor, informal_salutation)
@@ -69,17 +84,49 @@ class MIS_Staff
     true
   end
 
+  def self.from_pass(record)
+    MIS_Staff.new.from_pass(record)
+  end
+
+  def self.from_yaml(record, datasource_id)
+    MIS_Staff.new.from_yaml(record, datasource_id)
+  end
+
   def self.construct(loader, mis_data)
     @staff_by_name = Hash.new
     staff_by_id = Hash.new
     mis_data[:timetable_records].each do |record|
-      staff_by_id[record.staff_id] ||= MIS_Staff.new(record)
+      staff_by_id[record.staff_id] ||= MIS_Staff.from_pass(record)
+    end
+    #
+    #  Can we get any extra staff records?  Need a suitable datasource
+    #  first.
+    #
+    extra_staff = Array.new
+    yaml_datasource = Datasource.find_by(name: "Yaml")
+    if yaml_datasource
+      begin
+        extra_staff_records =
+          YAML.load(
+            File.open(Rails.root.join('import/pass/Extra', 'staff.yml'))
+        )
+        extra_staff_records.each do |record|
+          extra_staff << MIS_Staff.from_yaml(record, yaml_datasource.id)
+        end
+      rescue Errno::ENOENT
+        #
+        #  If there's no file, then we simply don't load it.
+        #
+      end
     end
     @staff_by_name = Hash.new
     staff_by_id.each do |id, staff|
       @staff_by_name[staff.formal_name] = staff
     end
-    staff_by_id.values
+    extra_staff.each do |staff|
+      @staff_by_name[staff.formal_name] = staff
+    end
+    staff_by_id.values + extra_staff
   end
 
   def self.by_name(name)
