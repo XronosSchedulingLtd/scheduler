8d3fba2350bd79c172557bbe54a6b20ee78bb85e
John Winters <john@xronos.uk>
2018-08-20 10:42:54 +0100

    Update location display name on alias change

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @location1 = locations(:roomone)
+    @aliasedroom = locations(:aliasedroom)
+  end
+
+  test "modifying location name should change element name" do
+    org_name = @location1.element.name
+    @location1.name = "Banana"
+    @location1.save
+    assert_not_equal org_name, @location1.element.name
+  end
+
+  test "saving location should modify element name" do
+    #
+    #  The location already has a display alias, but the element name
+    #  (as set up through fixtures) does not reflect this.
+    #
+    org_name = @aliasedroom.element.name
+    @aliasedroom.save
+    assert_not_equal org_name, @aliasedroom.element.name
+  end
 end
