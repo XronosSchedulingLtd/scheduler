934706032f9a9f72114b7cc7aa3e70b55800b802
John Winters <john@xronos.uk>
2019-04-10 10:20:28 +0100

    Add user UUIDs when migrating

--- db/migrate/20190326091128_add_api_fields.rb
+++ db/migrate/20190326091128_add_api_fields.rb
@@ -3,5 +3,10 @@ class AddApiFields < ActiveRecord::Migration
     add_column :users, :uuid, :string
     add_index  :users, :uuid, unique: true
     add_column :users, :can_api, :boolean, default: false
+    reversible do |change|
+      change.up do
+        User.generate_uuids
+      end
+    end
   end
 end
