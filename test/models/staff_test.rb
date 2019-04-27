d6ebfdf8c269a3e10f79ff374e8ac22163bb5612
John Winters <john@xronos.uk>
2019-04-27 13:45:59 +0100

    Allow editing of viewable flag

--- test/models/staff_test.rb
+++ test/models/staff_test.rb
@@ -1,12 +1,24 @@
 require 'test_helper'
 
 class StaffTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
   test "viewable should default to true" do
     staff = FactoryBot.create(:staff)
     assert staff.element.viewable?
   end
 
+  test "can create staff with viewable false" do
+    staff = FactoryBot.create(:staff, edit_viewable: false)
+    assert_not staff.element.viewable?
+  end
+
+  test "string \"0\" should work as false too" do
+    staff = FactoryBot.create(:staff, edit_viewable: "0")
+    assert_not staff.element.viewable?
+  end
+
+  test "and \"1\" should work as true" do
+    staff = FactoryBot.create(:staff, edit_viewable: "1")
+    assert staff.element.viewable?
+  end
 end
