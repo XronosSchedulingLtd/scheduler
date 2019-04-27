5b91b27f86b3145db45f2b37014367b8073a2050
John Winters <john@xronos.uk>
2019-04-27 14:15:52 +0100

    And corresponding tests

--- test/models/setting_test.rb
+++ test/models/setting_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class SettingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should be a setting for busy text" do
+    assert Setting.respond_to?(:busy_string)
+  end
+
+  test "default busy string should be \"Busy\"" do
+    assert_equal "Busy", Setting.busy_string
+  end
+
 end
