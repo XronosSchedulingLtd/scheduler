81861741ec7f8c8fc87cea3a04cabfcf76377f2b
John Winters <john@xronos.uk>
2019-10-01 14:45:28 +0100

    More tests on user permissions

--- test/models/user_profile_test.rb
+++ test/models/user_profile_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class UserProfileTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #
+  #  Really just testing that our test environment has been set up
+  #  correctly.
+  #
+  test "staff profile has correct permissions" do
+    staff_should_have = [
+      :editor,
+      :can_repeat_events,
+      :can_add_resources,
+      :can_add_notes,
+      :can_has_groups,
+      :public_groups,
+      :can_find_free,
+      :can_add_concerns,
+      :can_roam,
+      :can_has_files
+    ]
+    check_permission_set(UserProfile.staff_profile, staff_should_have)
+  end
+
+  test "pupil profile has correct permissions" do
+    pupil_should_have = [
+      :editor
+    ]
+    check_permission_set(UserProfile.pupil_profile, pupil_should_have)
+  end
+
+  test "guest profile has correct permissions" do
+    guest_should_have = [
+    ]
+    check_permission_set(UserProfile.guest_profile, guest_should_have)
+  end
+
+  private
+
+  def check_permission_set(profile, should_have)
+    PermissionFlags::KNOWN_PERMISSIONS.each do |key|
+      value = profile.permissions[key]
+      if should_have.include?(key)
+        assert_equal PermissionFlags::PERMISSION_YES, value, "Should have #{key}"
+      else
+        assert_equal PermissionFlags::PERMISSION_NO, value, "Should not have #{key}"
+      end
+    end
+  end
+
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,6 +10,124 @@ class UserTest < ActiveSupport::TestCase
     assert_equal user.corresponding_staff, staff
   end
 
+  test "new staff user gets correct permissions" do
+    #
+    #  Note that here we're really just checking that the user
+    #  has been linked to the right profile, and then that the
+    #  permissions have propagated through in the correct way.
+    #
+    #  The flags in the profiles are set up by a fixture when
+    #  in test mode, and by code in the UserProfile model for
+    #  real systems.  I have yet to find a way to unify these two.
+    #
+    staff = FactoryBot.create(:staff, email: 'able@baker.com')
+    user = FactoryBot.create(:user, email: 'able@baker.com')
+    assert user.editor?
+    assert user.can_repeat_events?
+    assert user.can_add_resources?
+    assert user.can_add_notes?
+    assert user.can_has_groups?
+    assert user.public_groups?
+    assert user.can_find_free?
+    assert user.can_add_concerns?
+    assert user.can_roam?
+    assert user.can_has_files?
+
+    assert_not user.admin?
+    assert_not user.edit_all_events?
+    assert_not user.subedit_all_events?
+    assert_not user.privileged?
+    assert_not user.can_has_forms?
+    assert_not user.can_su?
+    assert_not user.exams?
+    assert_not user.can_relocate_lessons?
+    assert_not user.can_view_forms?
+    assert_not user.can_view_unconfirmed?
+    assert_not user.can_edit_memberships?
+    assert_not user.can_api?
+  end
+
+  test "new pupil user gets correct permissions" do
+    #
+    #  Note that here we're really just checking that the user
+    #  has been linked to the right profile, and then that the
+    #  permissions have propagated through in the correct way.
+    #
+    #  The flags in the profiles are set up by a fixture when
+    #  in test mode, and by code in the UserProfile model for
+    #  real systems.  I have yet to find a way to unify these two.
+    #
+    pupil = FactoryBot.create(:pupil, email: 'able@baker.com')
+    user = FactoryBot.create(:user, email: 'able@baker.com')
+    assert user.editor?
+
+    assert_not user.can_repeat_events?
+    assert_not user.can_add_resources?
+    assert_not user.can_add_notes?
+    assert_not user.can_has_groups?
+    assert_not user.public_groups?
+    assert_not user.can_find_free?
+    assert_not user.can_add_concerns?
+    assert_not user.can_roam?
+    assert_not user.can_has_files?
+    assert_not user.admin?
+    assert_not user.edit_all_events?
+    assert_not user.subedit_all_events?
+    assert_not user.privileged?
+    assert_not user.can_has_forms?
+    assert_not user.can_su?
+    assert_not user.exams?
+    assert_not user.can_relocate_lessons?
+    assert_not user.can_view_forms?
+    assert_not user.can_view_unconfirmed?
+    assert_not user.can_edit_memberships?
+    assert_not user.can_api?
+  end
+
+  test "new guest user gets correct permissions" do
+    #
+    #  Note that here we're really just checking that the user
+    #  has been linked to the right profile, and then that the
+    #  permissions have propagated through in the correct way.
+    #
+    #  The flags in the profiles are set up by a fixture when
+    #  in test mode, and by code in the UserProfile model for
+    #  real systems.  I have yet to find a way to unify these two.
+    #
+    user = FactoryBot.create(:user, email: 'able@baker.com')
+
+    assert_not user.editor?
+    assert_not user.can_repeat_events?
+    assert_not user.can_add_resources?
+    assert_not user.can_add_notes?
+    assert_not user.can_has_groups?
+    assert_not user.public_groups?
+    assert_not user.can_find_free?
+    assert_not user.can_add_concerns?
+    assert_not user.can_roam?
+    assert_not user.can_has_files?
+    assert_not user.admin?
+    assert_not user.edit_all_events?
+    assert_not user.subedit_all_events?
+    assert_not user.privileged?
+    assert_not user.can_has_forms?
+    assert_not user.can_su?
+    assert_not user.exams?
+    assert_not user.can_relocate_lessons?
+    assert_not user.can_view_forms?
+    assert_not user.can_view_unconfirmed?
+    assert_not user.can_edit_memberships?
+    assert_not user.can_api?
+  end
+
+  test "can add specific permission for new user" do
+    staff = FactoryBot.create(:staff, email: 'able@baker.com')
+    user = FactoryBot.create(:user, :admin, email: 'able@baker.com')
+    assert user.admin?
+    assert_equal PermissionFlags::PERMISSION_NO,
+                 user.user_profile.permissions[:admin]
+  end
+
   test "don't link to non-current staff" do
     staff = FactoryBot.create(:staff, email: 'able@baker.com', current: false)
     user = FactoryBot.create(:user, email: 'able@baker.com')
