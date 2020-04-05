3bbc0c4420b770545f61c729a1f3581afc83a7fd
John Winters <john@xronos.uk>
2020-04-05 18:20:34 +0100

    Make last three migrations Rails 5 compatible
    They now need a version number on the end of them.

--- db/migrate/20200116122918_add_locking.rb
+++ db/migrate/20200116122918_add_locking.rb
@@ -1,4 +1,4 @@
-class AddLocking < ActiveRecord::Migration
+class AddLocking < ActiveRecord::Migration[4.2]
   def change
     add_column :properties, :locking, :boolean, default: false
     add_column :events, :locked, :boolean, default: false
--- db/migrate/20200312090509_add_zoom_fields.rb
+++ db/migrate/20200312090509_add_zoom_fields.rb
@@ -1,4 +1,4 @@
-class AddZoomFields < ActiveRecord::Migration
+class AddZoomFields < ActiveRecord::Migration[4.2]
   def change
     add_column :settings, :zoom_link_text,     :string
     add_column :settings, :zoom_link_base_url, :string
