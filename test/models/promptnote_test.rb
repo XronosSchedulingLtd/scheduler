15ecded8f6891a03b34144f52447385fca4c661c
John Winters <john@xronos.uk>
2020-04-10 16:42:10 +0100

    Add tests for promptnotes

--- test/models/promptnote_test.rb
+++ test/models/promptnote_test.rb
@@ -15,14 +15,32 @@ class PromptnoteTest < ActiveSupport::TestCase
     }
   end
 
-  test "can create prompt note" do
+  test 'can create prompt note' do
     pn = Promptnote.new(@valid_params)
     assert pn.valid?
   end
 
-  test "must have an element" do
+  test 'must have an element' do
     pn = Promptnote.new(@valid_params.except(:element))
     assert_not pn.valid?
+  end
+
+  test 'can have notes' do
+    pn = Promptnote.create(@valid_params)
+    pn.notes << FactoryBot.create(:note)
+    pn.notes << FactoryBot.create(:note)
+    assert_equal 2, pn.notes.count
+  end
+
+  test 'deleting the promptnote orphans the notes' do
+    pn = Promptnote.create(@valid_params)
+    pn.notes << pn0 = FactoryBot.create(:note)
+    pn.notes << pn1 = FactoryBot.create(:note)
+    pn.destroy
+    pn0.reload
+    pn1.reload
+    assert_nil pn0.promptnote
+    assert_nil pn1.promptnote
   end
 
 end
