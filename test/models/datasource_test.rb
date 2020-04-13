f8b17c973214255e90ed4dca84041709b3cd69f0
John Winters <john@xronos.uk>
2020-04-13 12:13:16 +0100

    Add tests for datasource model

--- test/models/datasource_test.rb
+++ test/models/datasource_test.rb
@@ -14,21 +14,102 @@ class DatasourceTest < ActiveSupport::TestCase
     }
   end
 
-  test "can create a datasource" do
+  test 'can create a datasource' do
     ds = Datasource.new(@valid_params)
     assert ds.valid?
   end
 
-  test "name is required" do
+  test 'name is required' do
     ds = Datasource.new(@valid_params.except(:name))
     assert_not ds.valid?
   end
 
-  test "name must be unique" do
+  test 'name must be unique' do
     ds1 = Datasource.create(@valid_params)
     assert ds1.valid?
     ds2 = Datasource.new(@valid_params)
     assert_not ds2.valid?
+  end
+
+  test 'can have staff members' do
+    ds = Datasource.create(@valid_params)
+    ds.staffs << staff1 = FactoryBot.create(:staff)
+    ds.staffs << staff2 = FactoryBot.create(:staff)
+    assert_equal 2, ds.staffs.count
+    ds.destroy
+    assert_not staff1.destroyed?
+    assert_not staff2.destroyed?
+    staff1.reload
+    staff2.reload
+    assert_nil staff1.datasource
+    assert_nil staff2.datasource
+  end
+
+  test 'can have pupils' do
+    ds = Datasource.create(@valid_params)
+    ds.pupils << pupil1 = FactoryBot.create(:pupil)
+    ds.pupils << pupil2 = FactoryBot.create(:pupil)
+    assert_equal 2, ds.pupils.count
+    ds.destroy
+    assert_not pupil1.destroyed?
+    assert_not pupil2.destroyed?
+    pupil1.reload
+    pupil2.reload
+    assert_nil pupil1.datasource
+    assert_nil pupil2.datasource
+  end
+
+  test 'can have locationaliases' do
+    ds = Datasource.create(@valid_params)
+    ds.locationaliases << locationalias1 = FactoryBot.create(:locationalias)
+    ds.locationaliases << locationalias2 = FactoryBot.create(:locationalias)
+    assert_equal 2, ds.locationaliases.count
+    ds.destroy
+    assert_not locationalias1.destroyed?
+    assert_not locationalias2.destroyed?
+    locationalias1.reload
+    locationalias2.reload
+    assert_nil locationalias1.datasource
+    assert_nil locationalias2.datasource
+  end
+
+  test 'can have groups' do
+    ds = Datasource.create(@valid_params)
+    ds.groups << group1 = FactoryBot.create(:group)
+    ds.groups << group2 = FactoryBot.create(:group)
+    assert_equal 2, ds.groups.count
+    ds.destroy
+    assert_not group1.destroyed?
+    assert_not group2.destroyed?
+    group1.reload
+    group2.reload
+    assert_nil group1.datasource
+    assert_nil group2.datasource
+  end
+
+  test 'sorts by name' do
+    datasources = Array.new
+    datasources << ds1 = Datasource.create(@valid_params.merge({name: "Zak"}))
+    datasources << ds2 = Datasource.create(@valid_params.merge({name: "Able"}))
+    datasources << ds3 = Datasource.create(@valid_params.merge({name: "Baker"}))
+    sorted = datasources.sort
+    assert_equal ds2, sorted[0]
+    assert_equal ds3, sorted[1]
+    assert_equal ds1, sorted[2]
+  end
+
+  test 'prevent destruction if pupils' do
+    ds = Datasource.create(@valid_params)
+    assert ds.can_destroy?
+    ds.pupils << pupil1 = FactoryBot.create(:pupil)
+    assert_not ds.can_destroy?
+  end
+
+  test 'prevent destruction if staff' do
+    ds = Datasource.create(@valid_params)
+    assert ds.can_destroy?
+    ds.staffs << staff1 = FactoryBot.create(:staff)
+    assert_not ds.can_destroy?
   end
 
 end
