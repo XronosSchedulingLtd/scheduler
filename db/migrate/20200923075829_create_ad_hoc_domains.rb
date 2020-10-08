27508d0dac97a076137b7697e3b5ffb157db34c3
John Winters <john@sinodun.org.uk>
2020-10-08 10:38:18 +0100

    And two more (which I could have sworn I had committed)

--- db/migrate/20200923075829_create_ad_hoc_domains.rb
+++ db/migrate/20200923075829_create_ad_hoc_domains.rb
@@ -6,15 +6,46 @@ class CreateAdHocDomains < ActiveRecord::Migration[5.2]
       t.integer :eventcategory_id
       t.integer :connected_property_element_id
       t.integer :default_day_shape_id
+      t.integer :datasource_id
       t.timestamps
     end
 
     create_table :ad_hoc_domain_controllers do |t|
       t.integer :ad_hoc_domain_id
       t.integer :user_id
+      t.timestamps
     end
 
     add_index :ad_hoc_domain_controllers, :ad_hoc_domain_id
     add_index :ad_hoc_domain_controllers, :user_id
+
+    create_table :ad_hoc_domain_subjects do |t|
+      t.integer :ad_hoc_domain_id
+      t.integer :subject_element_id
+      t.timestamps
+    end
+    add_index :ad_hoc_domain_subjects, :ad_hoc_domain_id
+
+    create_table :ad_hoc_domain_staffs do |t|
+      t.integer :ad_hoc_domain_id
+      t.integer :staff_element_id
+      t.integer :subject_element_id
+      t.timestamps
+    end
+    add_index :ad_hoc_domain_staffs, :ad_hoc_domain_id
+
+    create_table :ad_hoc_domain_pupil_courses do |t|
+      t.integer :ad_hoc_domain_id
+      t.integer :pupil_element_id
+      t.integer :subject_element_id
+      t.integer :staff_element_id
+      t.integer :lessons_per_week, default: 1
+      t.timestamps
+    end
+    add_index :ad_hoc_domain_pupil_courses, :ad_hoc_domain_id
+    add_index :ad_hoc_domain_pupil_courses, :pupil_element_id
+    add_index :ad_hoc_domain_pupil_courses, :subject_element_id
+    add_index :ad_hoc_domain_pupil_courses, :staff_element_id
+
   end
 end
