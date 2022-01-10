cd345ab047355a8eb5c5458c928e3e0dac18e92b
John Winters <john@sinodun.org.uk>
2022-01-10 11:06:58 +0000

    Implement second allocation strategy
    Takes into account the hit which each pupil will take by
    missing any particular slot.

--- app/services/auto_allocator.rb
+++ app/services/auto_allocator.rb
@@ -29,6 +29,9 @@ class AutoAllocator
       @cost      = cost
     end
 
+    def dump
+      Rails.logger.debug("    Cost #{@cost} at #{time_slot.to_s} on #{date}")
+    end
   end
 
   class PupilPotentials < Array
@@ -42,14 +45,26 @@ class AutoAllocator
       super()
       @pid  = pc.pupil_id
       @name = pc.name
+      @lowest_cost = nil
     end
 
     def add_entry(date, time_slot, cost)
+      if @lowest_cost.nil? || cost < @lowest_cost
+        @lowest_cost = cost
+      end
       self << Potential.new(date, time_slot, cost)
     end
 
     def count_slots_at(cost)
       self.select {|p| p.cost == cost}.count
+    end
+
+    #
+    #  How many slots does this student have at his or her current
+    #  lowest cost.
+    #
+    def count_lowest_cost_slots
+      count_slots_at(@lowest_cost)
     end
 
     def summary
@@ -66,6 +81,39 @@ class AutoAllocator
 
     def best_score
       self.collect(&:cost).min
+    end
+
+    def dump
+      Rails.logger.debug("  #{@pid} #{@name}")
+      self.sort_by(&:cost).each do |potential|
+        potential.dump
+      end
+    end
+
+    def cost_of_missing(score)
+      #
+      #  If we miss out on a slot with the indicated score, how much
+      #  worse is our next best one?
+      #
+      scores = self.collect(&:cost).sort
+      #
+      #  It's tempting to use "scores.delete" but that will take
+      #  away *all* instances of the indicated score.
+      #
+      index = scores.index(score)
+      if index
+        scores.delete_at(index)
+      end
+      if scores.empty?
+        999
+      else
+        #
+        #  The first entry in the array is now our best score.
+        #  If that's less than what we were asked about, then we
+        #  will return a negative value.
+        #
+        scores[0] - score
+      end
     end
 
   end
@@ -91,6 +139,22 @@ class AutoAllocator
         9999
       end
     end
+
+    def cost_of_pupil_missing(pid, score)
+      pupil_potentials = self[pid]
+      if pupil_potentials
+        pupil_potentials.cost_of_missing(score)
+      else
+        9999
+      end
+    end
+
+    def dump
+      self.each do |pid, pupil_potentials|
+        pupil_potentials.dump
+      end
+    end
+
   end
 
   class OtherAllocations < Hash
@@ -363,7 +427,188 @@ class AutoAllocator
 
   end
 
+  class UnallocatedSet
+    #
+    #  Add some extra functions to an array of unallocated pupil courses
+    #  to enable manipulation.
+    #
+    def initialize(loadings, pcs)
+      #
+      #  We will be taking things out of this array so do a shallow
+      #  duplication.
+      #
+      @pcs = pcs.dup
+      @loadings = loadings
+    end
+
+    def eliminate_over!(mins)
+      @pcs = @pcs.select {|pc| pc.mins <= mins}
+    end
+
+    def eliminate_clashes!(date, time_slot)
+      @pcs = @pcs.select { |pc|
+        would_use = time_slot.trim_to(pc.mins.minutes)
+        !@loadings.other_allocations[pc.pupil_id]&.any_clashes_with?(date,
+                                                                     would_use)
+      }
+    end
+
+    def eliminate_better_elsewhere!(date, time_slot)
+      @pcs = @pcs.select { |pc|
+        Rails.logger.debug("Considering #{pc.name}")
+        @loadings.best_possible_score(pc) >=
+          @loadings.cost_for(pc, date, time_slot)
+      }
+    end
+
+    def select_lowest_cost!(date, time_slot)
+      #
+      #  Select all the pupil courses which share an equal lowest
+      #  cost for this lot.  That is - work out the lowest possible
+      #  cost of occupying this slot and select all the pcs which would
+      #  have that cost.
+      #
+      lowest_cost = 999
+      chosen = []
+      @pcs.each do |pc|
+        cost = @loadings.cost_for(pc, date, time_slot)
+        if cost < lowest_cost
+          #
+          #  We have a new lowest cost
+          #
+          lowest_cost = cost
+          chosen = [pc]
+        elsif cost == lowest_cost
+          #
+          #  Add one to our selection
+          #
+          chosen << pc
+        end
+      end
+      @pcs = chosen
+      return lowest_cost
+    end
+
+    def select_highest_penalty_of_missing!(date, time_slot)
+      #
+      #  For each candidate, work out how much *more* it would cost
+      #  this candidate if he or she didn't get this slot.  Select
+      #  all those with highest such cost.
+      #
+      highest_penalty = -999
+      chosen = []
+      @pcs.each do |pc|
+        cost = @loadings.cost_for(pc, date, time_slot)
+        penalty =
+          @loadings.all_potentials.cost_of_pupil_missing(pc.pupil_id, cost)
+        if penalty > highest_penalty
+          #
+          #  We have a new highest penalty
+          #
+          highest_penalty = penalty
+          chosen = [pc]
+        elsif penalty == highest_penalty
+          #
+          #  Add one to our selection
+          #
+          chosen << pc
+        end
+      end
+      @pcs = chosen
+    end
+
+    def select_best_for_pupil!(date, time_slot)
+      #
+      #  Pick all the pupil courses for which slot would be their
+      #  best current option.
+      #
+      chosen = []
+      @pcs.each do |pc|
+        cost = @loadings.cost_for(pc, date, time_slot)
+        if cost == @loadings.all_potentials.best_score_for(pc.pupil_id)
+          chosen << pc
+        end
+      end
+      @pcs = chosen
+    end
+
+    def least_flexible
+      #
+      #  Pick the unallocated pupil course which has the fewest available
+      #  slots at its minimum cost.
+      #
+      #  This used to take a cost as an argument and calculate the
+      #  fewest at that cost, but it's now more flexible.  If the
+      #  set has previously been adjusted so that they all have the
+      #  same lowest cost (by discarding any others) then it will work
+      #  as before, but it could also be used with a more mixed set, working
+      #  out which of them has the fewest at their individual lowest cost.
+      #
+      #  Sixth formers take priority, even if they are not the
+      #  least flexible.  Once we've chosen one, only another less
+      #  flexible sixth former can usurp the slot.
+      #
+      #  E.g. a sixth former has 3 slots with a cost of 0, whilst
+      #  a middle schooler has only 1 slot with a cost of 0, but other
+      #  possibilities available.
+      #
+      #  N.B.  "unallocated" array may be empty.
+      #
+      fewest = 999
+      chosen = nil
+      chosen_inflexible_one = false
+      #
+      self.each do |pc|
+        #
+        #  If we have already chosen an inflexible pupil and this latest
+        #  one is flexible then we don't consider him or her.
+        #
+        unless chosen_inflexible_one && pc.can_miss
+          possibilities =
+            @loadings.all_potentials[pc.pupil_id].count_lowest_cost_slots
+          if possibilities < fewest
+            fewest = possibilities
+            chosen = pc
+            if !pc.can_miss
+              chosen_inflexible_one = true
+            end
+          end
+        end
+      end
+      chosen
+    end
+
+    def size
+      @pcs.size
+    end
+
+    #
+    #  Probably want to eliminate this as a public method by
+    #  moving the functionality which calls it into this object
+    #
+    def select(&block)
+      selected = @pcs.select(&block)
+      UnallocatedSet.new(@loadings, selected)
+    end
+
+    #
+    #  Likewise
+    #
+    def each
+      @pcs.each do |pc|
+        yield pc
+      end
+    end
+
+    def empty?
+      @pcs.empty?
+    end
+
+  end
+
   class Loadings
+    attr_reader :other_allocations, :all_potentials
+
     #
     #  An object to hold a set of loadings and recalculate
     #  them on request.  To do this it needs to control both
@@ -437,7 +682,7 @@ class AutoAllocator
       #
       allocated_pcids =
         @allocated.allocations_in_week(date).collect {|a| a.pcid}.uniq
-      @pupil_courses.except(allocated_pcids)
+      UnallocatedSet.new(self, @pupil_courses.except(allocated_pcids))
     end
 
     def all_allocated_in_week?(date)
@@ -461,7 +706,7 @@ class AutoAllocator
       @all_potentials.best_score_for(pc.pupil_id)
     end
 
-    def best_choice_for(date, time_slot)
+    def best_choice_for1(date, time_slot)
       unallocated = unallocated_in_week(date)
       #
       #  "unallocated" is the initial list of pupil courses which we
@@ -482,15 +727,11 @@ class AutoAllocator
       #  cost, or we'll come back to this slot once those lower cost
       #  points have been eliminated.
       #
-      unallocated = unallocated.select {|ua| ua.mins <= time_slot.mins}
+      unallocated.eliminate_over!(time_slot.mins)
       #
       #  Does any student have a clashing "other commitment"?
       #
-      unallocated = unallocated.select {|ua|
-        would_use = time_slot.trim_to(ua.mins.minutes)
-        !@other_allocations[ua.pupil_id]&.any_clashes_with?(date,
-                                                            would_use)
-      }
+      unallocated.eliminate_clashes!(date, time_slot)
       #
       #  Now we want to select all the lowest cost ones.
       #
@@ -499,33 +740,42 @@ class AutoAllocator
       #  of 999.  All sixth formers with an academic clash are thus
       #  discarded at this point.
       #
-      lowest_cost = 999
-      chosen = []
-      unallocated.each do |ua|
-        cost = cost_for(ua, date, time_slot)
-        if cost < lowest_cost
-          #
-          #  We have a new lowest cost
-          #
-          lowest_cost = cost
-          chosen = [ua]
-        elsif cost == lowest_cost
-          #
-          #  Add one to our selection
-          #
-          chosen << ua
-        end
-      end
-      unallocated = chosen
+      unallocated.select_lowest_cost!(date, time_slot)
       #
       #  Could any of these do better elsewhere?
       #  Note this is not just "better in our slot", but anywhere better
       #  in the whole of what's left.
       #
-      unallocated = unallocated.select { |pc|
-        best_possible_score(pc) >= cost_for(pc, date, time_slot)
-      }
-      chosen = least_flexible(unallocated, lowest_cost)
+      unallocated.eliminate_better_elsewhere!(date, time_slot)
+      #
+      #  And take the least flexible.
+      #
+      chosen = unallocated.least_flexible
+
+      if chosen
+        Rails.logger.debug("Chose #{chosen.name} (#{chosen.pupil_id}) at a cost of #{cost_for(chosen, date, time_slot)}")
+      else
+        Rails.logger.debug("Didn't pick anyone")
+      end
+      chosen
+    end
+
+    def best_choice_for(date, time_slot)
+      unallocated = unallocated_in_week(date)
+      #
+      #  Uncontroversial stuff
+      #
+      unallocated.eliminate_over!(time_slot.mins)
+      unallocated.eliminate_clashes!(date, time_slot)
+      #
+      #  Now a slightly different approach.
+      #
+      unallocated.select_best_for_pupil!(date, time_slot)
+      unallocated.select_highest_penalty_of_missing!(date, time_slot)
+      unallocated.select_lowest_cost!(date, time_slot)
+
+      chosen = unallocated.least_flexible
+
       if chosen
         Rails.logger.debug("Chose #{chosen.name} (#{chosen.pupil_id}) at a cost of #{cost_for(chosen, date, time_slot)}")
       else
@@ -608,18 +858,54 @@ class AutoAllocator
           end
         end
       end
-      Rails.logger.debug("Potentials")
-      Rails.logger.debug(@all_potentials.inspect)
+      #Rails.logger.debug("Potentials")
+      #Rails.logger.debug(@all_potentials.inspect)
     end
 
-    def dump_potentials(pupil_identifier)
+    def dump_potentials
       Rails.logger.debug("Candidates:")
-      @all_potentials.each do |pid, possibilities|
-        Rails.logger.debug("  #{pid} #{pupil_identifier.call(pid)}")
-        possibilities.each do |cost, count|
-          Rails.logger.debug("    #{count} slots at cost #{cost}")
+      @all_potentials.dump
+    end
+
+    def cost_for(pc, date, target_slot)
+      #
+      #  Calculate the cost for the pupil of putting an allocation for
+      #  this pupil course at this proposed time.
+      #
+      #  Note - we don't attempt to check it fits in the indicated
+      #  slot.  We use the duration of the allocation.  Calling code
+      #  should have already checked whether it fits.
+      #
+      cost = 0
+      slot = TimeSlot.new(target_slot.beginning,
+                          target_slot.beginning + pc.mins.minutes)
+      timetable = @timetables[pc.pupil_id]
+      loadings = @loadings_by_pid[pc.pupil_id]
+      if timetable
+        lessons = timetable.entries_on(date)
+        lessons.each do |lesson|
+          if slot.overlaps?(lesson.time_slot)
+            #
+            #  Is this student allowed to miss this lesson?
+            #
+            if pc.can_miss
+              #
+              #  This student is allowed to miss academic lessons
+              #
+              cost += (loadings[lesson.subject_id] + 1)
+            else
+              #
+              #  This student is not allowed to miss academic
+              #  lessons.  Is this lesson missable?
+              #
+              unless lesson.missable
+                cost += 1000
+              end
+            end
+          end
         end
       end
+      cost
     end
 
     private
@@ -666,47 +952,6 @@ class AutoAllocator
         end
       end
       chosen
-    end
-
-    def cost_for(pc, date, target_slot)
-      #
-      #  Calculate the cost for the pupil of putting an allocation for
-      #  this pupil course at this proposed time.
-      #
-      #  Note - we don't attempt to check it fits in the indicated
-      #  slot.  We use the duration of the allocation.  Calling code
-      #  should have already checked whether it fits.
-      #
-      cost = 0
-      slot = TimeSlot.new(target_slot.beginning,
-                          target_slot.beginning + pc.mins.minutes)
-      timetable = @timetables[pc.pupil_id]
-      loadings = @loadings_by_pid[pc.pupil_id]
-      if timetable
-        lessons = timetable.entries_on(date)
-        lessons.each do |lesson|
-          if slot.overlaps?(lesson.time_slot)
-            #
-            #  Is this student allowed to miss this lesson?
-            #
-            if pc.can_miss
-              #
-              #  This student is allowed to miss academic lessons
-              #
-              cost += (loadings[lesson.subject_id] + 1)
-            else
-              #
-              #  This student is not allowed to miss academic
-              #  lessons.  Is this lesson missable?
-              #
-              unless lesson.missable
-                cost += 1000
-              end
-            end
-          end
-        end
-      end
-      cost
     end
 
     def calculate_all
@@ -975,7 +1220,7 @@ class AutoAllocator
                                          @other_engagements,
                                          effective_dates,
                                          jump_by)
-          @loadings.dump_potentials(method(:pupil_identifier))
+          @loadings.dump_potentials
           #
           #  Now work through all our dates, trying to allocate something.
           #
@@ -1100,24 +1345,6 @@ class AutoAllocator
         end
       end
     end
-  end
-
-  def pupil_identifier(pid)
-    #
-    #  Cacheing method to enable conversion of a pid to a name in a
-    #  relatively efficient way.  Used only for debug code.
-    #
-    name = @pid_cache[pid]
-    unless name
-      pupil = Pupil.find_by(id: pid)
-      if pupil
-        @pid_cache[pid] = pupil.name
-      else
-        @pid_cache[pid] = "<unknown>"
-      end
-      name = @pid_cache[pid]
-    end
-    name
   end
 
 end
