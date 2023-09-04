b737826b05078f178656b487b582e9f119fa4b88
John Winters <john@sinodun.org.uk>
2023-09-04 11:57:21 +0100

    Cope with extra yeargroups for Abingdon girls
    Abingdon school have entered some girls from SHSK and given them
    national curriculum years of 16 and 17 in iSAMS.  This is a frig
    in iSAMS to cope with its inability to handle disparate groups
    of pupils.
    
    Enhanced import code to cope with it.

--- lib/import/school/Abingdon/misutils.rb
+++ lib/import/school/Abingdon/misutils.rb
@@ -5,6 +5,10 @@ module MIS_Utils
   def local_yeargroup(nc_year)
     if nc_year == 15
       5
+    elsif nc_year == 16
+      6
+    elsif nc_year == 17
+      7
     else
       nc_year - 6
     end
@@ -17,11 +21,18 @@ module MIS_Utils
   alias local_yeargroup_text_pupils local_yeargroup_text
 
   def local_effective_start_year(era, nc_year, ahead = 0)
-    era.starts_on.year + 7 - (nc_year + ahead)
+    era.starts_on.year + 1 - (local_yeargroup(nc_year) + ahead)
   end
 
   def local_wanted(nc_year)
-    nc_year < 20
+    #
+    #  This test really should be just "< 20" in order to exclude the
+    #  prep school but various frigs have occurred.
+    #
+    #  15, 16, and 17 have been used for odd pupils in 5th, L6th and U6th
+    #  respectively.  See local_yeargroup() above.
+    #
+    nc_year < 18
   end
 
   #
